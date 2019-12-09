91605ebeae5300a859092383f44ff136b9fede52
shicks1080 <stuart.hicks@gmail.com>
2019-12-09 14:47:56 +0100

    Initialising socket io
    Setting things in place so socket.io should work.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-// Routes of the pages being served
-app.get('', (req, res) => {
-  res.render('index', {
-    title: 'Chat App'
-  })
+io.on('connection', () => {
+  console.log('New WebSocket connection')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('Server is on port ' + port + '.')
 })
