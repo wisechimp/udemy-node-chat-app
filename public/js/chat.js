44d272eb9beeac849ab9a2559b93d75e6bed4d03
shicks1080 <stuart.hicks@gmail.com>
2019-12-10 17:20:17 +0100

    Beginning to restrict messages to rooms
    Needed to reorder the javascript in chat.html as it wasn't accessing the scripts in a helpful order!
    Parsing the location to get username and room.
    Using the above to restrict  the message when someone enters a room to that room.

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,6 +11,11 @@ const $messages = document.querySelector('#messages')
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-template').innerHTML
 
+//  Options
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+})
+
 socket.on('message', (message) => {
   console.log(message)
   const html = Mustache.render(messageTemplate, {
@@ -68,3 +73,5 @@ $shareLocationButt.addEventListener('click', () => {
     })
   })
 })
+
+socket.emit('join', { username, room })
--- src/index.js
+++ src/index.js
@@ -19,29 +19,40 @@ app.use(express.static(publicDirectoryPath))
 
 let welcomeMessage = 'Welcome!'
 
+// The various communication functions available to the app
 io.on('connection', (socket) => {
   console.log('New WebSocket connection')
 
-  socket.emit('message', generateMessage(welcomeMessage))
+  // When someone joins a room
+  socket.on('join', ({ username, room }) => {
+    socket.join(room)
 
-  socket.broadcast.emit('message', generateMessage('A new user has joined'))
+    // Notifying everyone else in the room that someone has joined
+    socket.emit('message', generateMessage(welcomeMessage))
+    socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined ${room}`))
+  })
 
+  // When someone sends a message
   socket.on('sendMessage', (message, callback) => {
     const filter = new Filter()
 
+    // A filter to prevent naughty words
     if (filter.isProfane(message)) {
       return callback('Fing error!')
     }
 
-    io.emit('message', generateMessage(message))
+    // Sending the message to everyone is the words are OK
+    io.to('With a View').emit('message', generateMessage(message))
     callback()
   })
 
+  // Sharing the user's location link
   socket.on('userLocation', (coords, callback) => {
     socket.broadcast.emit('locationLink', generateLocationLink(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
     callback()
   })
 
+  // Disconnecting from the chat
   socket.on('disconnect', () => {
     io.emit('message', generateMessage('A user has left'))
   })
